2a77d2159aa3d0b71462c1e87789db1f85eb1ffd
sumitnagrikar <sumitnagrikar1@gmail.com>
2024-12-03 13:06:53 +0530

    update image responsive ness for lg screen

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -22,7 +22,7 @@ const BestSeller = () => {
         </p>
       </div>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6 px-4 ml-6 sm:px-6 md:px-10'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
         {
             bestSeller.map((item,index)=>(
                 <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -21,14 +21,16 @@ const LatestCollection = () => {
           </p>
       </div>
 
-      {/* Rendering Products */}
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6 px-4 ml-6 sm:px-6 md:px-10'>
-        {
-          latestProducts.map((item,index)=>(
-            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
-          ))
-        }
-      </div>
+{/* Rendering Products */}
+<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+  {
+    latestProducts.map((item, index) => (
+      <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+    ))
+  }
+</div>
+
+
     </div>
   )
 }
